cd579dddcc8689979507c06c2d0e6d78f0098ab3
sdswoops824 <swagata.das.2013@gmail.com>
2021-07-30 12:02:32 +0900

    preprocessing - min max scaling

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ input_data = np.array([[2.1, -1.9, 5.5],
                        [-1.5, 2.4, 3.5],
                        [0.5, -7.9, 5.6],
                        [5.9, 2.3, -5.8]])
+print("\nOriginal array: \n", input_data)
 
 # Binarizarion
 data_binarized = preprocessing.Binarizer(threshold=0.5).transform(input_data)
@@ -33,7 +34,12 @@ print("Std deviation = ", input_data.std(axis = 0))
 
 # remove mean and std
 data_scaled = preprocessing.scale(input_data)
-print("Mean = ", data_scaled.mean(axis =0))
-print("Std deviation = ", data_scaled.std(axis = 0))
+print("New_Mean = ", data_scaled.mean(axis =0))
+print("New_Std deviation = ", data_scaled.std(axis = 0))
+
+# min max scaling
+data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0,1))
+data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
+print("\nMin max scaled data:\n", data_scaled_minmax)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
