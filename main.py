bbcbdcaf79de1339539b8aab539c7291f9f01191
sdswoops824 <swagata.das.2013@gmail.com>
2021-07-30 11:45:10 +0900

    preprocessing - binarization

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
+from sklearn import preprocessing
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +15,14 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+input_data = np.array([[2.1, -1.9, 5.5],
+                       [-1.5, 2.4, 3.5],
+                       [0.5, -7.9, 5.6],
+                       [5.9, 2.3, -5.8]])
+
+data_binarized = preprocessing.Binarizer(threshold=0.5).transform(input_data)
+print("\nBinarized Data:\n", data_binarized)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
