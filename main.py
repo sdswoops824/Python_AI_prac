206a304effe42a2074c9090ab5772c76e872afbc
sdswoops824 <swagata.das.2013@gmail.com>
2021-07-30 11:55:42 +0900

    preprocessing - mean removal

--- main.py
+++ main.py
@@ -21,8 +21,19 @@ input_data = np.array([[2.1, -1.9, 5.5],
                        [0.5, -7.9, 5.6],
                        [5.9, 2.3, -5.8]])
 
+# Binarizarion
 data_binarized = preprocessing.Binarizer(threshold=0.5).transform(input_data)
 print("\nBinarized Data:\n", data_binarized)
 
+
+# Mean Removal
+# display mean and std
+print("Mean = ", input_data.mean(axis = 0))
+print("Std deviation = ", input_data.std(axis = 0))
+
+# remove mean and std
+data_scaled = preprocessing.scale(input_data)
+print("Mean = ", data_scaled.mean(axis =0))
+print("Std deviation = ", data_scaled.std(axis = 0))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
